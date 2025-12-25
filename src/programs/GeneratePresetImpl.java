67bfb311535496bcf045d995227f64aea468fc92
kurindinvv <kurindinvv@ERV-W0013>
2025-12-25 16:17:43 +0300

    Initial commit

---
+++ src/programs/GeneratePresetImpl.java
@@ -0,0 +1,17 @@
+package programs;
+
+import com.battle.heroes.army.Army;
+import com.battle.heroes.army.Unit;
+import com.battle.heroes.army.programs.GeneratePreset;
+
+import java.util.*;
+
+public class GeneratePresetImpl implements GeneratePreset {
+
+    @Override
+    public Army generate(List<Unit> unitList, int maxPoints) {
+        // Ваше решение
+        System.out.println(">>> STUB: GeneratePresetImpl.generate() called");
+        return new Army();
+    }
+}
---
+++ src/programs/SimulateBattleImpl.java
@@ -0,0 +1,16 @@
+package programs;
+
+import com.battle.heroes.army.Army;
+import com.battle.heroes.army.programs.PrintBattleLog;
+import com.battle.heroes.army.programs.SimulateBattle;
+
+public class SimulateBattleImpl implements SimulateBattle {
+    private PrintBattleLog printBattleLog; // Позволяет логировать. Использовать после каждой атаки юнита
+
+    @Override
+    public void simulate(Army playerArmy, Army computerArmy) throws InterruptedException {
+        // Ваше решение
+        System.out.println(">>> STUB: SimulateBattleImpl.simulate() called");
+        return;
+    }
+}
